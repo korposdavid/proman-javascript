41709d8bf053edc273de4d6bf86c70c60d48c6c2
Barnabas Bucsek <bucsekbarnabas@gmail.com>
2019-08-07 11:05:59 +0200

    add login functions

--- util.py
+++ util.py
@@ -1,5 +1,6 @@
 from functools import wraps
-from flask import jsonify
+from flask import jsonify, session, redirect, url_for
+import bcrypt
 
 
 def json_response(func):
@@ -11,5 +12,27 @@ def json_response(func):
     @wraps(func)
     def decorated_function(*args, **kwargs):
         return jsonify(func(*args, **kwargs))
+
+    return decorated_function
+
+
+
+def hash_password(plain_text_password):
+    # By using bcrypt, the salt is saved into the hash itself
+    hashed_bytes = bcrypt.hashpw(plain_text_password.encode('utf-8'), bcrypt.gensalt())
+    return hashed_bytes.decode('utf-8')
+
+
+def verify_password(plain_text_password, hash_to_verify):
+    hashed_bytes_password = hash_to_verify.encode('utf-8')
+    return bcrypt.checkpw(plain_text_password.encode('utf-8'), hashed_bytes_password)
+
+
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):        #USERNAME AND LOGIN WATCH OUT
+        if not session.get('username'):
+            return redirect(url_for('login'))
+        return f(*args, **kwargs)
 
     return decorated_function
